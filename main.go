cbe2aec389ecac11de4f6355df57ce282acab4ac
CatMoe <lykorisr@proton.me>
2022-08-14 20:45:02 +0800

    ✨ Change TLS port to try using CloudFlare

--- main.go
+++ main.go
@@ -33,8 +33,8 @@ func main() {
 	app.Post("/article/:aid/comment/create", handlers.ArticleContentCreateHandler)
 	app.Get("/article/:aid/comment/fetch/:startAcid", handlers.ArticleContentQueryHandler)
 
-	app.ListenTLS(":8888", "./data/tls/cert.pem", "./data/tls/key")
-	//app.Listen(":8888")
+	app.ListenTLS(":443", "./data/tls/cert.pem", "./data/tls/key")
+	//app.Listen(":443")
 
 	utils.GlobalLogger.Info("Running cleanup progress")
 	// Clean up
